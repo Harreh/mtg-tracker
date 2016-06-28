d0979aafe4e9e8d9d896b4dd0aac063f3defe634
Harry Dowe <harry.dowe@gmail.com>
2016-06-28 23:21:02 +0100

    Added basic favicon and tweaked inactive tab styling a tad.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
     <title>MTG Tracker</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">

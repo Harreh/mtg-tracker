38193cca647aa16bbe0cf7df14ab76b7deb2bb54
Harry Dowe <harry.dowe@gmail.com>
2016-06-30 20:51:02 +0100

    Removed some old bootstrap shit & changed poison's purple to green (cus reasons).

--- resources/views/bracket/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="col-md-offet-4 col-md-4">
-            <div id="bracket-container"></div>
-        </div>
-    </div>
-@endsection

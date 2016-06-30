26d869c1053c817b390bdad941b6700c1af98a0d
Harry Dowe <harry.dowe@gmail.com>
2016-06-30 20:25:39 +0100

    Tweaked some styling and shit.

--- resources/views/life/partials/player.blade.php
+++ resources/views/life/partials/player.blade.php
@@ -4,9 +4,9 @@
         <li class="poison-button player-tab"><i class="ss ss-nph"></i><span>Poison</span></li>
     </ul>
     <div class="player-content">
-        <div>
+        <div class="player-name-container">
             <span class="player-name" data-bind="text: $data.name"></span>
-            <input type="text" class="player-name hidden" data-bind="value: $data.name">
+            <input type="text" class="player-name-input hidden" data-bind="value: $data.name">
         </div>
         <div class="player-life text-active" data-bind="text: $data.life"></div>
 
@@ -14,12 +14,12 @@
 
         <div class="player-button-container">
             <div>
-                <button value="1" class="btn btn-add-life btn-xl"><i class="fa fa-plus" aria-hidden="true"></i>1</button>
-                <button value="5" class="btn btn-add-life btn-xl"><i class="fa fa-plus" aria-hidden="true"></i>5</button>
+                <button value="1" class="btn btn-success btn-add-life btn-xl"><i class="fa fa-plus" aria-hidden="true"></i>1</button>
+                <button value="5" class="btn btn-success btn-add-life btn-xl"><i class="fa fa-plus" aria-hidden="true"></i>5</button>
             </div>
             <div>
-                <button value="-1" class="btn btn-lose-life btn-xl"><i class="fa fa-minus" aria-hidden="true"></i>1</button>
-                <button value="-5" class="btn btn-lose-life btn-xl"><i class="fa fa-minus" aria-hidden="true"></i>5</button>
+                <button value="-1" class="btn btn-default btn-lose-life btn-xl"><i class="fa fa-minus" aria-hidden="true"></i>1</button>
+                <button value="-5" class="btn btn-default btn-lose-life btn-xl"><i class="fa fa-minus" aria-hidden="true"></i>5</button>
             </div>
         </div>
     </div>
